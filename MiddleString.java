63d772d067459344d8e28e9297440b7b73620e26
Luke Walker <LWalkerCS@lasalle.pvt.k12.or.us>
2017-09-12 09:29:09 -0700

    Update

---
+++ MiddleString.java
@@ -0,0 +1,16 @@
+import java.lang.*;
+public class MiddleString {
+  public static void main(String[] args) {
+    if (args.length != 1)
+      return;
+    String word = args[0];
+    double place = word.length()/2;
+    double remainder = place%1==0?.5:0;
+    int first = (int)(place-remainder);
+    int second = (int)(place+remainder);
+    StringBuilder sb = new StringBuilder(word);
+    sb.setCharAt(first,Character.toUpperCase(sb.charAt(first)));
+    sb.setCharAt(second,Character.toUpperCase(sb.charAt(second)));
+    System.out.println(sb.toString());
+  }
+}
