f20c95599982df63d68b7ad7f815ac28ce6832de
Luke Walker <LWalkerCS@lasalle.pvt.k12.or.us>
2017-09-19 09:32:33 -0700

    Updated

---
+++ blackjack/Player.java
@@ -0,0 +1,43 @@
+package blackjack;
+
+import java.lang.*;
+
+public class Player {
+
+    public static ArrayList<Player> players = new ArrayList<Player>();
+
+    private String name;
+    private int number, tokens;
+
+    public Player(String name) {
+        this.name = name;
+        this.number = players.size() + 1;
+        this.tokens = 0;
+
+        players.add(this);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public int getTokens() {
+        return tokens;
+    }
+
+    public void setTokens(int tokens) {
+        this.tokens = tokens;
+    }
+
+    public void addTokens(int tokens) {
+        this.tokens += tokens;
+    }
+
+    public void removeTokens(int tokens) {
+        this.tokens -= tokens;
+    }
+}
