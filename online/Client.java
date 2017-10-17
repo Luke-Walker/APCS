dd344325682a4e716726b447f3e1c718aecf456e
Luke-Walker <lukeryanwalker2000@gmail.com>
2017-10-17 08:35:11 -0700

    Updated

--- online/Client.java
+++ online/Client.java
@@ -3,4 +3,35 @@ import java.net.*;
 
 public class Client {
 
+    private Socket clientSocket;
+    private PrintWriter out;
+    private BufferedReader in;
+
+    public void startConnection(String ip, int port) throws IOException {
+        clientSocket = new Socket(ip, port);
+        out = new PrintWriter(clientSocket.getOutputStream(), true);
+        in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+    }
+
+    public String sendMessage(String msg) throws IOException {
+        out.println(msg);
+        String resp = in.readLine();
+        return resp;
+    }
+
+    public void stopConnection() throws IOException {
+        in.close();
+        out.close();
+        clientSocket.close();
+    }
+
+    public static void main(String[] args) throws IOException {
+        if (args.length < 2) {
+            return;
+        }
+
+        Client client = new Client();
+        client.startConnection(args[0], Integer.parseInt(args[1]));
+        String response = client.sendMessage("test 123");
+    }
 }
--- online/Server.java
+++ online/Server.java
@@ -8,22 +8,36 @@ public class Server {
     private PrintWriter out;
     private BufferedReader in;
 
-    public void start(int port) {
+    public void start(int port) throws IOException {
         serverSocket = new ServerSocket(port);
         clientSocket = serverSocket.accept();
         out = new PrintWriter(clientSocket.getOutputStream(), true);
         in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+
+        String inputLine;
+        while ((inputLine = in.readLine()) != null) {
+            if (".".equals(inputLine)) {
+                out.println("good bye");
+                break;
+            }
+            out.println(inputLine);
+        }
     }
 
-    public void stop() {
+    public void stop() throws IOException {
         in.close();
         out.close();
         clientSocket.close();
         serverSocket.close();
     }
 
-    public static void main(String[] args) {
-        Online server = new Online();
-        server.start(9090);
+    public static void main(String[] args) throws IOException {
+        if (args.length < 1) {
+            System.out.println("Port required as argument.");
+            return;
+        }
+
+        Server server = new Server();
+        server.start(Integer.parseInt(args[0]));
     }
 }
