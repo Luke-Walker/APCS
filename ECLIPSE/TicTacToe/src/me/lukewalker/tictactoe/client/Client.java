f448d89db89fc8c77f099a70f5f1d923dfc15eb3
Luke-Walker <lukeryanwalker2000@gmail.com>
2017-10-23 09:43:13 -0700

    Updated

---
+++ ECLIPSE/TicTacToe/src/me/lukewalker/tictactoe/client/Client.java
@@ -0,0 +1,10 @@
+package me.lukewalker.tictactoe.client;
+
+public class Client {
+
+	// https://stackoverflow.com/questions/22287439/java-server-with-multiclient-communication	
+	
+	public static void main(String[] args) {
+		
+	}
+}
---
+++ ECLIPSE/TicTacToe/src/me/lukewalker/tictactoe/server/Server.java
@@ -0,0 +1,8 @@
+package me.lukewalker.tictactoe.server;
+
+public class Server {
+
+	public static void main(String[] args) {
+		
+	}
+}
