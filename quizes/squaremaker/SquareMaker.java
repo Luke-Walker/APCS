c1dda5ab86df0cf6419becbff307c0bb3afee025
Luke-Walker <lukeryanwalker2000@gmail.com>
2018-01-22 09:08:41 -0800

    Updated

---
+++ quizes/squaremaker/SquareMaker.java
@@ -0,0 +1,19 @@
+public class SquareMaker {
+
+  public static void main(String[] args) {
+    Square sq1 = new Square();
+    Square sq2 = new Square(new Point(2,2), new Point(4,4));
+
+    System.out.println(sq1);
+    System.out.println(sq2);
+    System.out.println();   // q13
+
+    System.out.println();   // q14
+
+    sq1.;   // q15
+    System.out.println(sq1);
+
+  }
+
+
+}
