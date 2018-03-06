2ec73cfa93e7c937edeee4af0eb8731d22ad4e41
Luke-Walker <lukeryanwalker2000@gmail.com>
2018-03-06 09:23:52 -0800

    Updated

---
+++ Minecraft.java
@@ -0,0 +1,8 @@
+public class Minecraft {
+
+    public static void main(String[] args) {
+        int[][][] chunk = {{{0,0,0},{0,0,0},{0,0,0}},
+                           {{2,2,2},{2,2,2},{2,2,2}},
+                           {{1,1,1},{1,1,1},{1,1,1}}};
+    }
+}
