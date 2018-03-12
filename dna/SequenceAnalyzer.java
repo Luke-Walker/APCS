2a9c0d7ead12212c1e41915f16e7b7b15990df9c
Luke-Walker <lukeryanwalker2000@gmail.com>
2018-03-12 09:38:40 -0700

    Updated

--- dna/SequenceAnalyzer.java
+++ dna/SequenceAnalyzer.java
@@ -1,22 +1,50 @@
 public class SequenceAnalyzer {
 
-    public static final String[] organisms = {"TCCAAACCCAGCTCTATTTTAGTGGTCATGGGTTCTGGTCCCCCCGAGCC",
-                                              "TCCAAACCCAGCTCTAATTAAGTGGTCAAGCGTTCTGGTCCCCCCGAGGC",
-                                              "AAACCCCTCTAGTGGTCATCTGGTCCGCCGAGC"};
+    public static final String[][] organisms = {{"TCCAAACCCAGCTCTATTTTAGTGGTCATGGGTTCTGGTCCCCCCGAGCC"},
+                                                {"TCCAAACCCAGCTCTAATTAAGTGGTCAAGCGTTCTGGTCCCCCCGAGGC"},
+                                                {"AAACCCCTCTAGTGGTCATCTGGTCCGCCGAGC"}};
 
     public static void main(String[] args) {
         // #1
-        int similarities = 0;
-        for (int i = 0; i < organisms[0].length(); i++) {
-            char c = organisms[0].charAt(i);
-            if (c == organisms[1].charAt(i)) similarities++;
-        }
-
-        double percent = (double)similarities/organisms[0].length()*100;
-        System.out.println("Similarity Percentage between (" + organisms[0] + ") and (" + organisms[1] + "):");
-        System.out.println(percent + "%\n");
+        System.out.println("Similarity Percentage between (" + organisms[0][0] + ") and (" + organisms[1][0] + "):");
+        System.out.println(calculateSimilarity(organisms[0], organisms[1]) + "%\n\n");
 
         // #2
 
+
+        // #3
+
+
+        // #9
+        System.out.println("Longest Match between (" + organisms[0][0] + ") and (" + organisms[2][0] + "):");
+        System.out.println(longestMatch() + "\n\n");
+    }
+
+    // #5
+    public static double calculateSimilarity(String[] list1, String[] list2) {
+        int similarities = 0;
+        for (int i = 0; i < list1[0].length(); i++) {
+            char c = list1[0].charAt(i);
+            if (c == list2[0].charAt(i)) similarities++;
+        }
+
+        return (double)similarities/list1[0].length()*100;
+    }
+
+    // #9
+    public static int longestMatch() {
+        int best = 0;
+        int count = 0;
+        for (int i = 0; i < organisms[0][0].length(); i++) {
+            char c = organisms[0][0].charAt(i);
+            if (c == organisms[2][0].charAt(count)) {
+                count++;
+            } else {
+                if (count > best) best = count;
+                count = 0;
+            }
+        }
+
+        return best;
     }
 }
